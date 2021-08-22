5d4a87bc1cfc4e0f9b755bd1759e9f852b3b8c30
Yuji <yujiricardo@gmail.com>
2021-08-22 16:55:20 -0300

    chore: change imposrt

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient } from '../../protocols/http/http-post-client';
-import { AuthenticationParams } from '../../../domain/usecases/authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
