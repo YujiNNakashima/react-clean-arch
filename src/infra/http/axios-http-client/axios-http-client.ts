9140c26495bfe900192fa7014ea1057aab507d70
Yuji <yujiricardo@gmail.com>
2021-08-25 11:07:22 -0300

    feat: add body to post

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,10 @@
 import { HttpPostParams } from '../../../data/protocols/http/http-post-client';
 import axios from 'axios'
+import { AuthenticationParams } from '../../../domain/usecases/authentication';
 
 export default class AxiosHttpClient {
 
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios(params.url)
+  async post(params: HttpPostParams<AuthenticationParams>): Promise<void> {
+    await axios.post(params.url, params.body)
   }
 }
