554caacfcc04060af283e2f5135343af4749ae3e
Yuji <yujiricardo@gmail.com>
2021-08-26 09:20:19 -0300

    feat: ensure post return correctely

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,10 +1,10 @@
-import { HttpPostParams } from '../../../data/protocols/http/http-post-client';
+import { HttpPostParams, HttpPostClient } from '../../../data/protocols/http/http-post-client';
 import axios from 'axios'
 import { AuthenticationParams } from '../../../domain/usecases/authentication';
 import { HttpResponse } from '../../../data/protocols/http/http-response';
 import { AccountModel } from '../../../domain/models/account-model';
 
-export default class AxiosHttpClient {
+export default class AxiosHttpClient implements HttpPostClient<any, any> {
 
   async post(params: HttpPostParams<AuthenticationParams>): Promise<HttpResponse<AccountModel>> {
     const response = await axios.post(params.url, params.body)
